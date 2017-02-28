fd8102c553d1d720db3ec518573cef3a3e73931d
Kunal Bajpai <kunal.bajpai@gmail.com>
2017-02-28 17:46:06 -0600

    Final version

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,38 +1,71 @@
+var oFunctions = {
+    add: function(x, y) {
+        console.log("Add");
+    },
+    subtract: function(x, y) {
+        console.log("subtract");
+    },
+    foo: function(x, y, z) {
+        console.log("foo");
+    },
+    doSomethingComplicated: function(x) {
+        console.log("doSomethingComplicated");
+    }
+};
+
+function generateSubExpression(fname, args, subExpr) {
+    var retVal = '<div class="sub-expr column">';
+
+    retVal += '<div class="ui button">' + fname + '&nbsp;</div>';
+
+    console.log(subExpr);
+
+    if(typeof subExpr !== 'undefined') {
+        retVal += '<div style="border: 1px solid black; display: inline-block; margin-top: 1em; margin-bottom: 1em;">' + subExpr.html() + '</div>';
+        args--;        
+    }
+
+    for(var i = 0; i < args; i++) {
+        retVal += '<input type="text">&nbsp;';
+    }
+
+    return retVal;
+
+}
+
 $(document).ready(function() {
-    $('.ui.search').search({
-        type: 'category',
-        minCharacters: 3,
-        apiSettings: {
-            onResponse: function(githubResponse) {
-                var
-                    response = {
-                        results: {}
-                    };
-                // translate GitHub API response to work with search
-                $.each(githubResponse.items, function(index, item) {
-                    var
-                        language = item.language || 'Unknown',
-                        maxResults = 8;
-                    if (index >= maxResults) {
-                        return false;
-                    }
-                    // create new language category
-                    if (response.results[language] === undefined) {
-                        response.results[language] = {
-                            name: language,
-                            results: []
-                        };
-                    }
-                    // add result to category
-                    response.results[language].results.push({
-                        title: item.name,
-                        description: item.description,
-                        url: item.html_url
-                    });
-                });
-                return response;
-            },
-            url: '//api.github.com/search/repositories?q={query}'
+    var funcList = ["add", "subtract", "foo", "doSomethingComplicated"];
+
+    for (var i = funcList.length - 1; i >= 0; i--) {
+        $("#divFuncList").append('<div class="four wide column"><div class="ui button draggable-func" data-val="'+ oFunctions[funcList[i]].length +'" data-action="'+ oFunctions[funcList[i]].name +'">' + oFunctions[funcList[i]].name + '</div></div>');
+    }
+
+    $(".draggable-func").draggable({
+        revert: 'valid'
+    });
+
+    $( "#droppable" ).droppable({
+      classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+      },
+      drop: function( event, ui ) {
+        var axn = $(ui.draggable).attr("data-action");
+        var val = $(ui.draggable).attr("data-val");
+
+        console.log($(this).find(".sub-expr"));
+
+        if($(this).find(".sub-expr").length > 0) {
+            $(this)
+              .addClass( "ui-state-highlight" )
+              .find("#mainExpr")
+                .html(generateSubExpression(axn, val, $(this).find(".sub-expr")));
+        } else {
+            $(this)
+              .addClass( "ui-state-highlight" )
+              .find("#mainExpr")
+                .html(generateSubExpression(axn, val));
         }
+      }
     });
 });
